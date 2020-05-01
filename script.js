f0e7f848ff388a639c4b3922447f7b0a54ba8a2b
lukas1us <lukas1us@users.noreply.github.com>
2020-05-01 20:41:02 +0200

    Added fixet header, new video content and quote

---
+++ script.js
@@ -0,0 +1,12 @@
+var quotes = [
+    "„Mé IQ je 6000, to je jako 6000 učitelů tělocviku nebo 12000 hlídačů na parkovišti...“<br> - Holly",
+    "„Dneska vypadám opravdu k světu. Vypadat jen o chloupek líp, tak mě zakážou.“<br> - Kocour",
+    "„Musím přiznat, že jsem to zeslonil. To je jako zkonit, ale mnohem víc.“<br> - Holly",
+    "„Za ty tři miliony let chovám bláhovou naději, že lidstvo přestalo válčit, vyléčilo všechny nemoci a zjednodušilo formulář daňového přiznání.“<br> - Holly",
+    "„Tvůj mozek je rychlejší než děcko po prvním kari.“<br> - Rimmer",
+    "„Ty nepotíš pot, ale maďarskou omáčku!“<br> - Rimmer",
+    "„Budeme sebou házet jako kapka potu mezi půlkama cvičitelky aerobiku.“<br> - Lister",
+    "„Chovám bláhovou naději, že za tři miliony let, co jsme pryč, lidstvo přestalo válčit, vyléčilo všechny choroby a podařilo se mu zjednodušit formulář daňového přiznání.“ <br> - Holly"
+    ];
+var div = document.getElementById('quote');
+div.innerHTML = div.innerHTML += quotes[0];
